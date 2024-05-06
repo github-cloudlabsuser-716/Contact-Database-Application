d5aa3a07e6314c939a367b6ab806861a9b11804c
github_cloudlabsuser_716@cloudlabsaiuser.com <github_cloudlabsuser_716@cloudlabsaiuser.com>
2024-05-06 20:55:55 +0000

    The most significant changes involve the `UserController.cs` and `Index.cshtml` files. In `UserController.cs`, the `Index` method has been updated to support user search functionality by name. Additionally, the `Details`, `Create`, `Edit`, and `Delete` methods have been implemented, which were previously empty. In `Index.cshtml`, a form has been added to facilitate user search by name, and the user list display has been updated. Other important changes include the update to the `CRUD application 2.sln` file, where the Visual Studio version was updated and a new project was added. The `Web.config` file was also updated to change the `oldVersion` attribute of the `bindingRedirect` element for `WebGrease`.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,4 +1,5 @@
 using CRUD_application_2.Models;
+using System;
 using System.Linq;
 using System.Web.Mvc;
  
@@ -8,59 +9,108 @@ namespace CRUD_application_2.Controllers
     {
         public static System.Collections.Generic.List<User> userlist = new System.Collections.Generic.List<User>();
         // GET: User
-        public ActionResult Index()
+        //public ActionResult Index()
+        //{
+        //    return View(userlist);
+        //}
+
+        // GET: Users
+        public ActionResult Index(string searchString = null)
         {
-            // Implement the Index method here
+            var users = from u in userlist select u;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                users = users.Where(s => s.Name.Contains(searchString));
+            }
+
+            return View(users.ToList());
         }
- 
+
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
-            // Implement the details method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
  
         // GET: User/Create
         public ActionResult Create()
         {
-            //Implement the Create method here
+            return View();
         }
  
         // POST: User/Create
         [HttpPost]
         public ActionResult Create(User user)
         {
-            // Implement the Create method (POST) here
+            if (ModelState.IsValid)
+            {
+                userlist.Add(user);
+                return RedirectToAction("Index");
+            }
+            return View(user);
         }
  
-        // GET: User/Edit/5
+      // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
-            // This method is responsible for displaying the view to edit an existing user with the specified ID.
-            // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
  
         // POST: User/Edit/5
         [HttpPost]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                return HttpNotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                //existingUser.Age = user.Age;
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
         }
  
-        // GET: User/Delete/5
+      // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
-            // Implement the Delete method here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user);
         }
  
-        // POST: User/Delete/5
+      // POST: User/Delete/5
         [HttpPost]
         public ActionResult Delete(int id, FormCollection collection)
         {
-            // Implement the Delete method (POST) here
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return HttpNotFound();
+            }
+
+            userlist.Remove(user);
+            return RedirectToAction("Index");
         }
     }
 }
